e11c898a58a97572b79660c73e143ee11cfbaed5
jrg96 <gomezlopez.jorge96@gmail.com>
2020-02-23 17:54:03 -0600

    redirect after login/logout

--- DateApp-SPA/src/app/nav/nav.component.ts
+++ DateApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,8 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertifyService: AlertifyService) { }
+  constructor(public authService: AuthService, private alertifyService: AlertifyService,
+    private router: Router) { }
 
   ngOnInit() {
   }
@@ -20,6 +22,8 @@ export class NavComponent implements OnInit {
       this.alertifyService.success('Login successfully');
     }, error => {
       this.alertifyService.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
     console.log(this.model);
   }
@@ -30,6 +34,7 @@ export class NavComponent implements OnInit {
 
   logout() {
     localStorage.removeItem('token');
+    this.router.navigate(['/home']);
     this.alertifyService.message('Logged out');
   }
 }
