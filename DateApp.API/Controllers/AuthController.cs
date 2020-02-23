be00ae3b54b193dea59b5be5675bee91976ba431
jrg96 <gomezlopez.jorge96@gmail.com>
2020-02-23 08:19:54 -0600

    Implemented JWT auth

--- DateApp.API/Controllers/AuthController.cs
+++ DateApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DateApp.API.Data;
 using DateApp.API.DTO;
 using DateApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DateApp.API.Controllers
 {
@@ -12,12 +18,14 @@ namespace DateApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _authRepository;
-        public AuthController(IAuthRepository authRepository)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository authRepository, IConfiguration config)
         {
             this._authRepository = authRepository;
+            this._config = config;
         }
 
-        // POST api/auth/
+        // POST api/auth/register
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDTO userForRegisterDTO)
         {
@@ -54,5 +62,56 @@ namespace DateApp.API.Controllers
         }
 
 
+        // POST api/auth/login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDTO userForLoginDTO)
+        {
+            /*
+             * ---------------------------------------------------------------------------
+             * ZONA DE VALIDACION
+             * ---------------------------------------------------------------------------
+             */
+
+            // Comprobamos si existe un usuario en la DB con esos credenciales
+            var user = await _authRepository.Login(userForLoginDTO.Username.ToLower(), userForLoginDTO.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            /*
+             * --------------------------------------------------------------------------
+             * ZONA DE PROCESAMIENTO DE LA PETICION
+             * --------------------------------------------------------------------------
+             */
+            
+            // Creando los claims del token JWT
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // Retornando la respuesta
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
+        }
+        
     }
 }
---
+++ DateApp.API/DTO/UserForLoginDTO.cs
@@ -0,0 +1,8 @@
+namespace DateApp.API.DTO
+{
+    public class UserForLoginDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
