76f1828156f34365ee3efb397b8ca1001d463020
jrg96 <gomezlopez.jorge96@gmail.com>
2020-02-23 10:41:50 -0600

    Show/Hide login form if user is logged in

--- DateApp-SPA/src/app/nav/nav.component.ts
+++ DateApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,12 +9,27 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
-  login(){
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Log successful');
+    }, error => {
+      console.log('Login failed');
+    });
     console.log(this.model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Logged out');
   }
 }
